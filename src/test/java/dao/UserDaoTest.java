d93ca69edcc3041eb9d37b5691a600878d882861
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-20 11:38:48 +0900

    spring 적용

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -5,22 +5,32 @@ import connectionmaker.ConnectionMaker;
 import domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
 
+    @Autowired
+    ApplicationContext context;
+
     @Test
-    @DisplayName("insert")
+    @DisplayName("insert and select")
     void insertAndSelect() throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new AWSConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        User user = new User("101","영지안","1234");
+        UserDao userDao = context.getBean("AWSUserDao",UserDao.class);
+
+        User user = new User("200","영지안","1234");
         userDao.insert(user);
 
-        User user2 = userDao.selectId("101");
+        User user2 = userDao.selectId("200");
         assertEquals("영지안",user2.getName());
     }
 }
