bc919b9c426e79d92198dd2381e9dad5a5861a01
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-20 14:28:08 +0900

    before each 적용, selectId 테스트

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -3,11 +3,14 @@ package dao;
 import connectionmaker.AWSConnectionMaker;
 import connectionmaker.ConnectionMaker;
 import domain.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,44 +25,53 @@ class UserDaoTest {
     @Autowired
     ApplicationContext context;
 
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+
+    @BeforeEach
+    void setUp() throws SQLException, ClassNotFoundException {
+        this.userDao = context.getBean("AWSUserDao", UserDao.class);
+        this.user1 = new User("1", "Choco", "913");
+        this.user2 = new User("2", "Chicon", "214");
+        this.user3 = new User("3", "Ppang", "1210");
+        userDao.deleteAll();
+    }
+
     @Test
     @DisplayName("insert and select")
     void insertAndSelect() throws SQLException, ClassNotFoundException {
-        User user1 = new User("1","Choco","913");
-        User user2 = new User("2","Chicon","214");
-        User user3 = new User("3","Ppang","1210");
 
-        UserDao userDao = context.getBean("AWSUserDao",UserDao.class);
-
-        userDao.deleteAll();
-        assertEquals(0,userDao.getCount());
+        assertEquals(0, userDao.getCount());
 
         userDao.insert(user1);
-        assertEquals(1,userDao.getCount());
+        assertEquals(1, userDao.getCount());
 
         User user = userDao.selectId("1");
-        assertEquals(user1.getName(),user.getName());
-        assertEquals(user1.getPassword(),user.getPassword());
+        assertEquals(user1.getName(), user.getName());
+        assertEquals(user1.getPassword(), user.getPassword());
     }
 
     @Test
     void count() throws SQLException, ClassNotFoundException {
 
-        User user1 = new User("1","Choco","913");
-        User user2 = new User("2","Chicon","214");
-        User user3 = new User("3","Ppang","1210");
-
-        UserDao userDao = context.getBean("AWSUserDao",UserDao.class);
-
-        userDao.deleteAll();
-
         userDao.insert(user1);
-        assertEquals(1,userDao.getCount());
+        assertEquals(1, userDao.getCount());
 
         userDao.insert(user2);
-        assertEquals(2,userDao.getCount());
+        assertEquals(2, userDao.getCount());
 
         userDao.insert(user3);
-        assertEquals(3,userDao.getCount());
+        assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    @DisplayName("selectId")
+    void selectId() throws SQLException, ClassNotFoundException {
+        userDao.insert(user1);
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            userDao.selectId("5");
+        });
     }
 }
